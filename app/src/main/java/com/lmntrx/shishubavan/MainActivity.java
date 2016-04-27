ab18298dc6685d1372db26883d19987bb94418f9
Nihal Ismail <nihalismailk@gmail.com>
2016-04-28 00:01:27 +0530

    Glitchy Front end

--- app/src/main/java/com/lmntrx/shishubavan/MainActivity.java
+++ app/src/main/java/com/lmntrx/shishubavan/MainActivity.java
@@ -1,10 +1,33 @@
 package com.lmntrx.shishubavan;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.nfc.Tag;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.GridLayout;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
+    public void addButton(View view)
+    {
+        GridLayout gridLayout=(GridLayout)findViewById(R.id.gridLayout);
+        ImageView dangerButton=new ImageView(this);
+        dangerButton.setImageResource(R.drawable.danger);
+        dangerButton.setLayoutParams(new ActionBar.LayoutParams(300,300));
+        gridLayout.addView(dangerButton);
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
