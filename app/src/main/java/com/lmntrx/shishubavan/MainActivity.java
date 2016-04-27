86bb916985a2654e5ae81b7abc9f68624a407b72
Nihal Ismail <nihalismailk@gmail.com>
2016-04-28 01:54:37 +0530

    Added Call feature to buttons
    Calls made to 123456789

--- app/src/main/java/com/lmntrx/shishubavan/MainActivity.java
+++ app/src/main/java/com/lmntrx/shishubavan/MainActivity.java
@@ -2,6 +2,7 @@ package com.lmntrx.shishubavan;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
@@ -16,17 +17,39 @@ import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.GridLayout;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    public void addButton(View view)
-    {
-        GridLayout gridLayout=(GridLayout)findViewById(R.id.gridLayout);
-        ImageView dangerButton=new ImageView(this);
+    public void addButton(final View view) {
+        GridLayout gridLayout = (GridLayout) findViewById(R.id.gridLayout);
+        ImageView dangerButton = new ImageView(this);
         dangerButton.setImageResource(R.drawable.danger);
-        dangerButton.setLayoutParams(new ActionBar.LayoutParams(300,300));
+        dangerButton.setLayoutParams(new ActionBar.LayoutParams(300, 300));
+        dangerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                makeCall(v);
+            }
+        });
         gridLayout.addView(dangerButton);
+    }
 
+    public void makeCall(View view) {
+        Log.i("Status", "call made");
+        Uri number = Uri.parse("tel:123456789");
+        Intent callIntent = new Intent(Intent.ACTION_CALL, number);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        startActivity(callIntent);
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
